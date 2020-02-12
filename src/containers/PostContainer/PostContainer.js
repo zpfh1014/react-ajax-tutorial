ac601bb70940ca68ee75177ab6a149478f997507
jykim <zpfhtmvlspf@gmail.com>
2020-02-12 18:02:50 +0900

    api add

--- src/containers/PostContainer/PostContainer.js
+++ src/containers/PostContainer/PostContainer.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import { PostWrapper, Navigate, Post } from '../../components';
-//import * as service from '../../services/post';
+import * as service from '../../services/post';
 
 
 class PostContainer extends Component {
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(data => this.setState({
-            posts: data
-        }));
-        //.then(data => console.log(data));
+        this.fetchPostInfo(1);
     }
 
-    // fetchPostInfo = async ( postId ) => {
-    //     const post = await service.getPost(postId);
-    //     console.log(post);
-    //     const comments = await service.getComments(postId);
-    //     console.log(comments);
-    // }
+    fetchPostInfo = async ( postId ) => {
+        // const post = await service.getPost(postId);
+        // console.log(post);
+        // const comments = await service.getComments(postId);
+        // console.log(comments);
+
+        const info = await Promise.all([
+            service.getPost(postId),
+            service.getComments(postId)
+        ]);
+
+        console.log(info);
+    }
 
     render() {
-        const { posts } = this.state;
+        //const { posts } = this.state;
         
         return(
             <PostWrapper>
@@ -34,3 +36,46 @@ class PostContainer extends Component {
 }
 
 export default PostContainer;
+
+
+
+
+// class PostContainer extends Component {
+//     state = {}
+
+//     componentDidMount() {
+//         this._getPosts();
+//     }
+
+//     _getPosts = async() => {
+//         const posts = await this._callApi();
+//         this.setState({
+//             posts
+//         })
+//     }
+
+//     _callApi = () => {
+//         return fetch('https://jsonplaceholder.typicode.com/posts')
+//         .then(res => res.json())
+//         .then(json => json.data.posts)
+//         .chtch(err => console.log(err))
+//     }
+
+//     _renderPosts = () => {
+//         const posts = this.state.posts.map(post => {
+//             return (
+//                 <Navigate postId={post.postId} disabled={post.fetching} />
+//                 <Post title={post.title} body={post.body} comments={post.comments} />
+//             )
+//         })
+//         return posts;
+//     }
+
+//     render() {
+//         return(
+//             <PostWrapper >
+//                 {this.state.posts ? this._renderPosts() : 'Loding'}
+//             </PostWrapper>
+//         )
+//     }
+// }
--- src/services/post.js
+++ src/services/post.js
@@ -1,7 +1,8 @@
-import axios from 'react';
+
+import axios from 'axios';
 
 export function getPost(postId) {
-    return axios.get('https://jsonplaceholder.typicode.com/posts' + postId);
+    return axios.get('https://jsonplaceholder.typicode.com/posts/' + postId);
 }
 
 export function getComments(postId) {
