8c5e818804addeaeb72922f5df160d0de645058b
jykim <zpfhtmvlspf@gmail.com>
2020-02-21 13:53:57 +0900

    add es6-promise

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './Animation.css';
 import 'semantic-ui-css/semantic.min.css';
+import 'es6-promise/auto';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
